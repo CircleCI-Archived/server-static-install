2b17d997b0f4a8b02b1ea50841391853b0f5f623
Danielle Tomlinson <dani@tomlinson.io>
2018-01-30 13:16:10 +0100

    Make docker install less trusty dependent

--- provision-services-ubuntu.sh
+++ provision-services-ubuntu.sh
@@ -42,7 +42,7 @@ run_installer(){
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
   add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
   apt-get update
-  apt-get -y install "docker-ce=${DOCKER_VERSION}~ce-0~ubuntu-trusty" cgmanager
+  apt-get -y install "docker-ce=${DOCKER_VERSION}~ce-0~ubuntu-$(lsb_release -cs)" cgmanager
 
   echo "--------------------------------------------"
   echo "          Downloading Replicated"
