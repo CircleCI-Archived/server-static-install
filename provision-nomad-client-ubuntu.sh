0e80d624be919774de4676d3202a360e8058fa06
Eugene C <eugene@circleci.com>
2018-05-09 17:42:49 -0700

    Change Docker install to use lsb_release
    Unhardcode Trusty for docker-ce

--- provision-nomad-client-ubuntu.sh
+++ provision-nomad-client-ubuntu.sh
@@ -36,7 +36,7 @@ apt-get install -y apt-transport-https ca-certificates curl
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
-apt-get -y install "docker-ce=${DOCKER_VERSION}~ce-0~ubuntu-trusty" cgmanager
+apt-get -y install "docker-ce=${DOCKER_VERSION}~ce-0~ubuntu-$(lsb_release -cs)" cgmanager
 
 echo "--------------------------------------"
 echo "         Installing nomad"
