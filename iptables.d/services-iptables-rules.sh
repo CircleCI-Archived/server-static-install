4d64d5b531b1dd9e7f070b9c0c9ac0e6bf379080
Marcus Stong <stongo@gmail.com>
2018-09-14 10:00:02 -0400

    remove iface

--- iptables.d/services-iptables-rules.sh
+++ iptables.d/services-iptables-rules.sh
@@ -1,23 +1,23 @@
 # See https://github.com/bear/iptable-tools for rules documentation
 
-inbound 22 ens3
-inbound 80 ens3
-inbound 443 ens3
-inbound 3001 ens3
-inbound 4647 ens3
-inbound 7171 ens3
-inbound 8081 ens3
-inbound 8125 ens3 UDP
-inbound 8585 ens3
-inbound 8800 ens3
+inbound 22
+inbound 80
+inbound 443
+inbound 3001
+inbound 4647
+inbound 7171
+inbound 8081
+inbound 8125 ${PUBLICNET} UDP
+inbound 8585
+inbound 8800
 
-outbound 80 ens3
-outbound 443 ens3
-outbound 4646 ens3
-outbound 4647 ens3
-outbound 4648 ens3
-outbound 5432 ens3
-outbound 5672 ens3
-outbound 6379 ens3
-outbound 15672 ens3
-outbound 27017 ens3
+outbound 80
+outbound 443
+outbound 4646
+outbound 4647
+outbound 4648
+outbound 5432
+outbound 5672
+outbound 6379
+outbound 15672
+outbound 27017
