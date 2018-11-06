d47368c8789943db2169bf2bd3d424899d79f77c
Michael Rosabal <mrosabal@circleci.com>
2018-11-06 15:14:00 -0500

    Require that scripts run as root
    Many of the installation steps and config changes require root level privileges.

--- provision-nomad-client-ubuntu.sh
+++ provision-nomad-client-ubuntu.sh
@@ -32,6 +32,12 @@ docker_package_name(){
   fi
 }
 
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
 echo "--------------------------------------------"
 echo "       Finding Private IP"
 echo "--------------------------------------------"
